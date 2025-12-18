7a5af33a5c0b4913056d42480e457f99ff53a498
aj99hall <aj99hall@gmail.com>
2025-12-18 19:11:04 +0000

    Handling composer 2 changes

--- terraform/modules/composer/main.tf
+++ terraform/modules/composer/main.tf
@@ -1,16 +1,40 @@
 resource "google_composer_environment" "composer_instance" {
-  name = var.name
+  provider = google-beta
+
+  name   = var.name
   region = var.region
 
   config {
-    environment_size = "ENVIRONMENT_SIZE_SMALL"
-
-    node_config {
-      machine_type = "e2-small"
-    }
+    environment_size = "ENVIRONMENT_SIZE_SMALL" # key cost lever :contentReference[oaicite:4]{index=4}
 
     software_config {
-      image_version = "composer-2-airflow-2"
+      image_version = var.image_version
+    }
+
+    # Composer 2 sizing is here (not node_config.machine_type) :contentReference[oaicite:5]{index=5}
+    workloads_config {
+      scheduler {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        count      = 1
+      }
+
+      web_server {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+      }
+
+      worker {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+
+        min_count  = 1
+        max_count  = 1
+      }
     }
   }
 }
+
