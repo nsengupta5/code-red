49d893f0601713b139113c82b033bb2a9c1a8b4b
aj99hall <aj99hall@gmail.com>
2025-12-18 21:51:53 +0000

    more service account iam tweaks

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,14 +1,28 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
 
+# Get project number (needed for Composer service agent)
+data "google_project" "current" {
+  project_id = var.project_id
+}
+
+# Composer runtime service account (used by Airflow workloads)
+resource "google_service_account" "composer" {
+  project      = var.project_id
+  account_id   = "composer-env-sa"
+  display_name = "Cloud Composer Runtime Service Account"
+}
+
+# Permissions for Airflow workers
 resource "google_project_iam_member" "composer_worker" {
   project = var.project_id
   role    = "roles/composer.worker"
   member  = "serviceAccount:${google_service_account.composer.email}"
 }
 
-data "google_project" "current" {
-  project_id = var.project_id
-}
-
+# REQUIRED: permissions for the Google-managed Composer service agent
 resource "google_project_iam_member" "composer_service_agent_ext" {
   project = var.project_id
   role    = "roles/composer.ServiceAgentV2Ext"
