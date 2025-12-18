027cc85bd3462bfb479fd6c75e627dd3db2ef093
aj99hall <aj99hall@gmail.com>
2025-12-18 19:30:18 +0000

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -20,3 +20,10 @@ provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
+
+module "composer" {
+  source = "../../modules/composer"
+
+  name   = "dev-composer"
+  region = var.region
+}
