3f632837c3264373429067d63cefdacfd910d997
aj99hall <aj99hall@gmail.com>
2025-12-18 21:48:14 +0000

    changing region to us
    quota starvation issue in europe-west2

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,8 +1,3 @@
-resource "google_service_account" "composer" {
-  project      = var.project_id
-  account_id   = "composer-env-sa"
-  display_name = "Cloud Composer Environment Service Account"
-}
 
 resource "google_project_iam_member" "composer_worker" {
   project = var.project_id
@@ -10,8 +5,12 @@ resource "google_project_iam_member" "composer_worker" {
   member  = "serviceAccount:${google_service_account.composer.email}"
 }
 
+data "google_project" "current" {
+  project_id = var.project_id
+}
+
 resource "google_project_iam_member" "composer_service_agent_ext" {
   project = var.project_id
   role    = "roles/composer.ServiceAgentV2Ext"
-  member  = "serviceAccount:${google_service_account.composer.email}"
+  member  = "serviceAccount:service-${data.google_project.current.number}@cloudcomposer-accounts.iam.gserviceaccount.com"
 }
