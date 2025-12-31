e4a26fdf09052a7c0dc7d9b9b5e6ac3c7b35464e
aj99hall <aj99hall@gmail.com>
2025-12-31 16:11:57 +0100

    Update csv_to_bq.py

--- dags/csv_to_bq.py
+++ dags/csv_to_bq.py
@@ -7,23 +7,20 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
-    render_template_as_native_obj=True,
 ) as dag:
 
     run_dataflow = BashOperator(
         task_id="run_dataflow_flex_template",
         bash_command="""
-        set -euo pipefail
-
-        gcloud dataflow flex-template run csv-to-bq-{{ ds_nodash }} \
+        gcloud dataflow flex-template run csv-to-bq-$(date +%Y%m%d) \
           --project project-990b8649-da36-4d4c-9d9 \
           --region us-central1 \
           --template-file-gcs-location gs://dataflow-staging-258083003066/templates/buggy-python-built.json \
           --service-account-email dataflow-worker@project-990b8649-da36-4d4c-9d9.iam.gserviceaccount.com \
           --temp-location gs://dataflow-temp-258083003066/temp \
           --staging-location gs://dataflow-staging-258083003066/staging \
-          --parameters input=gs://dummy-data-258083003066/input/sheep_colour_preferences.csv,output_table=project-990b8649-da36-4d4c-9d9:animal_facts.sheep_colour_preferences,error_table=project-990b8649-da36-4d4c-9d9:animal_facts.sheep_colour_bad_rows
-        """,
-        executable="/bin/bash",
+          --parameters input=gs://dummy-data-258083003066/input/sheep_colour_preferences.csv,\
+output_table=project-990b8649-da36-4d4c-9d9:animal_facts.sheep_colour_preferences,\
+error_table=project-990b8649-da36-4d4c-9d9:animal_facts.sheep_colour_bad_rows
+        """
     )
-
