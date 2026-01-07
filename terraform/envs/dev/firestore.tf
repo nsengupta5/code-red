1770fe9235c3c39c85763903fa2c2ff9bf1a4dce
aj99hall <aj99hall@gmail.com>
2026-01-07 09:15:49 +0100

    Firestore setup and slack notification dedupe

---
+++ terraform/envs/dev/firestore.tf
@@ -0,0 +1,13 @@
+resource "google_project_service" "firestore" {
+  project = var.project_id
+  service = "firestore.googleapis.com"
+}
+
+resource "google_firestore_database" "default" {
+  project     = var.project_id
+  name        = "(default)"
+  location_id = "nam5"
+  type        = "FIRESTORE_NATIVE"
+
+  depends_on = [google_project_service.firestore]
+}
