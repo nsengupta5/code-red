526bcf46fe2e7e4a635ca9fdffc442633615a58a
aj99hall <aj99hall@gmail.com>
2026-01-18 17:00:10 +0000

    Update monitoring_dashboards.tf

--- terraform/envs/dev/monitoring_dashboards.tf
+++ terraform/envs/dev/monitoring_dashboards.tf
@@ -102,27 +102,22 @@ resource "google_monitoring_dashboard" "dataflow_dashboard" {
       widgets = [
         # Dataflow aggregated worker utilization (all jobs in us-central1)
         {
-          title = "Aggregated worker utilization (%) – all Dataflow jobs (us-central1)"
-          xyChart = {
-            dataSets = [{
-              plotType = "LINE"
-              timeSeriesQuery = {
-                timeSeriesFilter = {
-                  filter = join(" AND ", [
-                    "resource.type=\"dataflow_job\"",
-                    "resource.labels.region=\"us-central1\"",
-                    "metric.type=\"dataflow.googleapis.com/job/aggregated_worker_utilization\""
-                  ])
-                  aggregation = {
-                    alignmentPeriod  = "60s"
-                    perSeriesAligner = "ALIGN_MEAN"
-                  }
+            title = "Aggregated worker utilization (%) – all Dataflow jobs"
+            xyChart = {
+                dataSets = [{
+                plotType = "LINE"
+                timeSeriesQuery = {
+                    timeSeriesQueryLanguage = <<-MQL
+                    fetch dataflow_job
+                    | metric 'dataflow.googleapis.com/job/aggregated_worker_utilization'
+                    | group_by [], mean(val())
+                    | every 60s
+                    MQL
                 }
-              }
-            }]
-            yAxis = { label = "%", scale = "LINEAR" }
-          }
-        },
+                }]
+                yAxis = { label = "%", scale = "LINEAR" }
+            }
+            },
 
         # Dataflow memory capacity (all jobs in us-central1)
         {
@@ -150,26 +145,26 @@ resource "google_monitoring_dashboard" "dataflow_dashboard" {
 
         # Dataflow OOM log-based metric (your custom metric)
         {
-          title = "Dataflow OOM errors (log-based metric)"
-          xyChart = {
-            dataSets = [{
-              plotType = "LINE"
-              timeSeriesQuery = {
-                timeSeriesFilter = {
-                  filter = join(" AND ", [
-                    "resource.type=\"global\"",
-                    "metric.type=\"logging.googleapis.com/user/dataflow_oom_errors\""
-                  ])
-                  aggregation = {
-                    alignmentPeriod  = "60s"
-                    perSeriesAligner = "ALIGN_DELTA"
-                  }
+            title = "Dataflow OOM errors (log-based metric)"
+            xyChart = {
+                dataSets = [{
+                plotType = "LINE"
+                timeSeriesQuery = {
+                    timeSeriesFilter = {
+                    filter = join(" AND ", [
+                        "resource.type=\"dataflow_step\"",
+                        "metric.type=\"logging.googleapis.com/user/dataflow_oom_errors\""
+                    ])
+                    aggregation = {
+                        alignmentPeriod  = "60s"
+                        perSeriesAligner = "ALIGN_DELTA"
+                    }
+                    }
                 }
-              }
-            }]
-            yAxis = { label = "count/min", scale = "LINEAR" }
-          }
-        }
+                }]
+                yAxis = { label = "count/min", scale = "LINEAR" }
+            }
+            }
       ]
     }
   })
